699667116dd59f9083c557d60ff934c6ab1ae2f3
Atakan Güloğlu <121863808+atakanguloglu@users.noreply.github.com>
2024-02-08 22:32:22 +0300

    table düzenlendi

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -1,5 +1,4 @@
-
-"use client"
+"use client";
 import React, { useState, useEffect } from 'react';
 import NavbarComponent from '@/components/Navbar';
 import axios from 'axios';
@@ -7,33 +6,43 @@ import './Dashboard.css';
 
 interface FormData {
   id: number;
-  temel_alan: string;
-  bilimsel_alan: string;
-  akademik_faaliyet_turu: string;
-  faaliyet: string;
-  eser_adi: string;
-  doi_numarasi: string;
-  kisi: string;
-  basvuru_donemi: string;
-  tesvik_puani: number;
-  basvuru_tarihi: string;
-  dosyalar: string;
-  onay_durumu: string;
+  submission_period: string;
+  name: string;
+  surname: string;
+  email: string;
+  title: string;
+  faculty: string;
+  department: string;
+  work_name: string;
+  basic_field: string;
+  scientific_field: string;
+  persons: number;
+  academic_activity_type: string;
+  activity: string;
+  doi_number: string;
+  file_path: string;
+  score: string;
+  status: string;
+  comment: string;
+  comment_by: string;
+  comment_date: string;
+  created_at: string;
+  updated_at: string;
 }
 
 const Dashboard = () => {
   const [formData, setFormData] = useState<FormData[]>([]);
-  const [loading, setLoading] = useState(true); // Başlangıçta veriler yükleniyor olarak ayarlanır
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(process.env.NEXT_PUBLIC_SERVER_URL + '/table');
         setFormData(response.data);
-        setLoading(false); // Veriler başarıyla alındıktan sonra yükleme durumu kapatılır
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
-        setLoading(false); // Hata durumunda da yükleme durumu kapatılır 
+        setLoading(false);
       }
     };
 
@@ -51,36 +60,45 @@ const Dashboard = () => {
             <thead>
               <tr>
                 <th>ID</th>
+                <th>Teslim Süresi</th>
+                <th>İsim</th>
+                <th>Soyisim</th>
+                <th>Email</th>
+                <th>Başlık</th>
+                <th>Fakülte </th>
+                <th>Bölüm</th>
+                <th>Eser Adı</th>
                 <th>Temel Alan</th>
                 <th>Bilimsel Alan</th>
-                <th>Akademik Faaliyet Türü</th>
-                <th>Faaliyet</th>
-                <th>Eser Adi</th>
-                <th>Doi Numarası</th>
-                <th>Kişi</th>
-                <th>Başvuru Dönemi</th>
-                <th>Teşvik Puanı</th>
-                <th>Başvuru Tarihi</th>
-                <th>Dosyalar</th>
-                <th>Onay Durumu</th>
+                <th>Kişi Sayısı</th>
+                <th>Akademik Faliyet Türü</th>
+                <th>Aktivite</th>
+                <th>DOI Numarası</th>
+                <th>Dosya</th>
+                <th>Status</th>
               </tr>
             </thead>
             <tbody>
               {formData.map(data => (
                 <tr key={data.id}>
                   <td>{data.id}</td>
-                  <td>{data.temel_alan}</td>
-                  <td>{data.bilimsel_alan}</td>
-                  <td>{data.akademik_faaliyet_turu}</td>
-                  <td>{data.faaliyet}</td>
-                  <td>{data.eser_adi}</td>
-                  <td>{data.doi_numarasi}</td>
-                  <td>{data.kisi}</td>
-                  <td>{data.basvuru_donemi}</td>
-                  <td>{data.tesvik_puani}</td>
-                  <td>{new Date(data.basvuru_tarihi).toLocaleDateString()}</td>
-                  <td>{data.dosyalar}</td>
-                  <td>{data.onay_durumu}</td>
+                  <td>{data.submission_period}</td>
+                  <td>{data.name}</td>
+                  <td>{data.surname}</td>
+                  <td>{data.email}</td>
+                  <td>{data.title}</td>
+                  <td>{data.faculty}</td>
+                  <td>{data.department}</td>
+                  <td>{data.work_name}</td>
+                  <td>{data.basic_field}</td>
+                  <td>{data.scientific_field}</td>
+                  <td>{data.persons}</td>
+                  <td>{data.academic_activity_type}</td>
+                  <td>{data.activity}</td>
+                  <td>{data.doi_number}</td>
+                  <td>{data.file_path}</td>
+                  <td>{data.score}</td>
+                  <td>{data.status}</td>
                 </tr>
               ))}
             </tbody>
