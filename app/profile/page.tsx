b9cd28c9800d0636b9a14275ef6d82b6c4e3c7c5
Atakan Güloğlu <121863808+atakanguloglu@users.noreply.github.com>
2024-02-12 01:57:08 +0300

    nex uı

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,11 @@
 "use client"
-import React, { useState } from 'react';
+
+import React, { useState, FormEvent } from 'react';
 import axios from 'axios';
 import NavbarComponent from '@/components/Navbar';
 import Image from 'next/image';
 import { FaUserCircle } from 'react-icons/fa';
+import { Input, Button } from '@nextui-org/react';
 const Page = () => {
     const [formData, setFormData] = useState({
         phone: '',
@@ -11,7 +13,7 @@ const Page = () => {
         email: ''
     });
 
-    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
             const response = await axios.put('/api/users', formData);
@@ -27,47 +29,44 @@ const Page = () => {
 
     return (
         <div>
-            <NavbarComponent />
-            <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
+          <NavbarComponent />
+          <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
             <Image
-        className='mx-auto p-4'
-        width={150}
-        height={66}
-        alt='logo'
-        src={'/logo-kucuk.png'}
-      />
-                    
-                
+              className='mx-auto p-4'
+              width={150}
+              height={66}
+              alt='logo'
+              src={'/logo-kucuk.png'}
+            />
+      
             <h2 className="text-2xl font-semibold mb-4 flex items-center">
-                    <FaUserCircle className="text-4xl text-gray-500 mr-1" /> {/* Avatar ikonu */}
-                    Kullanıcı Ayarları
-                </h2>
-                <div className="mb-4">
-                    <div className="flex flex-col">
-                        <label htmlFor="phone" className="text-gray-700 mb-1">Telefon Numarası:</label>
-                        <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} className="p-2 border rounded-md" />
-                        <button type="button" onClick={(e) => handleSubmit(e)} className="mt-2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Telefonu Güncelle</button>
-                    </div>
-                </div>
-                <div className="mb-4">
-                    <div className="flex flex-col">
-                        <label htmlFor="password" className="text-gray-700 mb-1">Şifre:</label>
-                        <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} className="p-2 border rounded-md" />
-                        <button type="button" onClick={(e) => handleSubmit(e)} className="mt-2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Şifreyi Güncelle</button>
-                    </div>
-                </div>
-                <div className="mb-4">
-                    <div className="flex flex-col">
-                        <label htmlFor="email" className="text-gray-700 mb-1">E-posta Adresi:</label>
-                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="p-2 border rounded-md" />
-                        <button type="button" onClick={(e) => handleSubmit(e)} className="mt-2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">E-postayı Güncelle</button>
-                    </div>
-                </div>
-            </div>
-            {/* Buraya sayfanın geri kalan kısmını ekleyebilirsiniz */}
+              <FaUserCircle className="text-4xl text-gray-500 mr-1" /> {/* Avatar ikonu */}
+              Kullanıcı Ayarları
+            </h2>
+      
+            <form onSubmit={handleSubmit} className='p-1'>
+              <div className='grid grid-cols-1 gap-4'>
+                <label htmlFor="phone" className="text-gray-700 mb-1">Telefon Numarası:</label>
+                <Input type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
+                <Button type="submit" className="mt-2" variant="flat" color="primary">Telefonu Güncelle</Button>
+              </div>
+      
+              <div className='grid grid-cols-1 gap-4'>
+                <label htmlFor="password" className="text-gray-700 mb-1">Şifre:</label>
+                <Input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
+                <Button type="submit" className="mt-2" variant="flat" color="primary">Şifreyi Güncelle</Button>
+              </div>
+      
+              <div className='grid grid-cols-1 gap-4'>
+                <label htmlFor="email" className="text-gray-700 mb-1">E-posta Adresi:</label>
+                <Input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+                <Button type="submit" className="mt-2" variant="flat" color="primary">E-postayı Güncelle</Button>
+              </div>
+            </form>
+          </div>
+          {/* Buraya sayfanın geri kalan kısmını ekleyebilirsiniz */}
         </div>
-    );
+      );
 };
 
 export default Page;
-    
