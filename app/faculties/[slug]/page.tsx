3bf894f605460ad20052d915c50132f12e3135e1
Atakan Güloğlu <121863808+atakanguloglu@users.noreply.github.com>
2024-02-17 03:34:19 +0300

    fakülte sayfası başlıkları çekme işlemi

--- app/faculties/[slug]/page.tsx
+++ app/faculties/[slug]/page.tsx
@@ -1,13 +1,15 @@
 'use client';
+// Page.tsx
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import NavbarComponent from '@/components/Navbar';
+
 const Page = () => {
   const pathname = usePathname();
   return (
     <div>
       <NavbarComponent />
-      {pathname && <div></div>}
+      {pathname && <h1>{pathname}</h1>}
     </div>
   );
 };
--- components/FacultiesMenu.tsx
+++ components/FacultiesMenu.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// NavbarComponent.tsx
+import React, { useState } from 'react';
 import {
   Dropdown,
   DropdownTrigger,
@@ -9,29 +10,46 @@ import {
 import { FacultiesAndDepartments } from '@/utils';
 import { useRouter } from 'next/navigation';
 
-export default function App() {
-  const router = useRouter();
-  return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button variant='light' className='!text-base p-0 m-0 max-w-fit'>
-          Fakülteler
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu
-        aria-label='Dynamic Actions'
-        items={FacultiesAndDepartments}
-      >
-        {(item) => (
-          <DropdownItem
-            key={item.faculty}
-            // color={item}
-            onClick={() => router.push(`/faculties/${item.slug}`)}
-          >
-            {item.faculty}
-          </DropdownItem>
-        )}
-      </DropdownMenu>
-    </Dropdown>
-  );
+interface Faculty {
+  faculty: string;
+  slug: string;
+  // Diğer özellikler...
 }
+
+const NavbarComponent = () => {
+  const router = useRouter();
+  const [selectedFaculty, setSelectedFaculty] = useState('');
+
+  const handleItemClick = (item: Faculty) => {
+    setSelectedFaculty(item.faculty);
+    router.push(`/faculties/${item.slug}`);
+  };
+
+  return (
+    <div>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant='light' className='!text-base p-0 m-0 max-w-fit'>
+            Fakülteler
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          aria-label='Dynamic Actions'
+          items={FacultiesAndDepartments}
+        >
+          {(item: Faculty) => (
+            <DropdownItem
+              key={item.faculty}
+              onClick={() => handleItemClick(item)}
+            >
+              {item.faculty}
+            </DropdownItem>
+          )}
+        </DropdownMenu>
+      </Dropdown>
+      {selectedFaculty && <h1>{selectedFaculty}</h1>}
+    </div>
+  );
+};
+
+export default NavbarComponent;
